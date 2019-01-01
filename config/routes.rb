9206fdee0d6b1610105fa48aa905497de5913ee4
ArtefactGitHub <artefactgo@gmail.com>
2019-01-01 18:44:05 +0900

    ユーザー認証処理の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
+
+  resources :user_sessions, only: %i[new create destroy]
+
   root to: 'rooms#show'
 
   # Serve websocket cable requests in-process
