973dc3804bec1ef151435dacee2ba946d4088b4a
ArtefactGitHub <artefactgo@gmail.com>
2019-01-03 18:25:39 +0900

    User、Message の関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :messages
+  has_many :chatrooms, through: :messages
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
-  validates :email, uniqueness: true
 end
