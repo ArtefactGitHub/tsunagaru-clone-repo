1f9ee66bbd7870a92d37c5afec211c0af7c5e4cb
ArtefactGitHub <artefactgo@gmail.com>
2019-01-03 18:21:04 +0900

    メッセージのバリデーションの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
   validates :content, length: { in: 1..99 }
+  validates :user_id, presence: true
+  validates :room_id, presence: true
 end
