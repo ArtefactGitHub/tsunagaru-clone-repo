aeec0defadefcc99a16e933fe0c16d413e79b177
ArtefactGitHub <artefactgo@gmail.com>
2019-01-05 11:16:30 +0900

    Room クラスに作成者の Owner のアソシエーションを持たせるマイグレーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_091703) do
+ActiveRecord::Schema.define(version: 2019_01_05_020803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_01_03_091703) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id"
+    t.index ["owner_id"], name: "index_rooms_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
