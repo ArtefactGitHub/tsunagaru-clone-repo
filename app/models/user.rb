cfed4345e517bf06d0de97c5506185a9355eca15
ArtefactGitHub <artefactgo@gmail.com>
2019-01-06 19:17:36 +0900

    User モデルに画像ファイル添付のためのマッピングを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :messages
   has_one :my_room, class_name: 'Room', foreign_key: 'owner_id'
   has_many :rooms, through: :messages
+  has_one_attached :avatar
 
   validates :name, presence: true
   validates :email, uniqueness: true
