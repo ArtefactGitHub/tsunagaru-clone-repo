541f7ca56fff63fa7e8114fb8e7e9624c9336205
ArtefactGitHub <artefactgo@gmail.com>
2019-01-11 18:28:47 +0900

    トモダチ申請処理の修正 ・申請時にステータスを更新 ・申請中・受理中のスコープから、承認済みを除外するよう修正 ・受理中で申請を行なった場合、そのまま承認処理を行うよう実装

--- app/controllers/mypage/friend/requests_controller.rb
+++ app/controllers/mypage/friend/requests_controller.rb
@@ -7,11 +7,21 @@ class Mypage::Friend::RequestsController < MypageController
   def create
     @request = FriendRequest.new(request_params)
     @request.own = current_user
+    @request.friend_request_status = :request
 
     if @request.save
-      redirect_to new_mypage_friend_request_path, success: 'トモダチ申請をしました'
+      receiving = FriendRequest.receiving_from(current_user, @request.opponent)
+      if receiving.present?
+        @request.update_approval!
+        receiving.update_approval!
+        redirect_to new_mypage_friend_request_path, success: 'トモダチ申請が承認されました'
+      else
+        redirect_to new_mypage_friend_request_path, success: 'トモダチ申請をしました'
+      end
     else
       flash.now[:danger] = 'トモダチ申請が出来ませんでした'
+      logger.debug @request.errors.full_messages if @request.present?
+
       set_new_request_params
       render :new
     end
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,6 @@
 class FriendRequest < ApplicationRecord
+  include FriendRequestStatus
+
   belongs_to :own, class_name: 'User'
   belongs_to :opponent, class_name: 'User'
 
@@ -6,15 +8,29 @@ class FriendRequest < ApplicationRecord
   validates :opponent_id, presence: true
   validates :own_id, uniqueness: { scope: :opponent_id }
   validate :can_not_request_to_own
+  validate :can_not_request_to_friend
 
   scope :sending_receiving, ->(own) { sending(own).or(receiving(own)) }
-  scope :sending, ->(own) { where(own_id: own.id) }
-  scope :receiving, ->(own) { where(opponent_id: own.id) }
+  scope :sending, ->(own) { where(own_id: own.id).where.not(friend_request_status: :approval) }
+  scope :receiving, ->(own) { where(opponent_id: own.id).where.not(friend_request_status: :approval) }
+  scope :receiving_from, ->(own, opponent) { receiving(own).where(own_id: opponent.id).where.not(friend_request_status: :approval).first }
+  scope :approvals, ->(own) { where(own_id: own.id).where(friend_request_status: :approval) }
+  scope :approval_to_by_id, ->(own_id, opponent_id) { where(own_id: own_id).where(opponent_id: opponent_id).where(friend_request_status: :approval) }
 
   def can_not_request_to_own
     if opponent_id == own_id
       errors.add(:opponent_id, '自分自身にトモダチ申請を送ることは出来ません')
     end
+  end
+
+  def can_not_request_to_friend
+    if FriendRequest.approval_to_by_id(own_id, opponent_id).present?
+      errors.add(:opponent_id, 'トモダチにトモダチ申請を送ることは出来ません')
+    end
+  end
+
+  def update_approval!
+    update!(friend_request_status: :approval)
   end
 
   def sending?(own)
