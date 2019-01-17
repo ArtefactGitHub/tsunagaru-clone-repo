233b149efc42e780c1c3b9488e0618a6679caa5d
ArtefactGitHub <artefactgo@gmail.com>
2019-01-17 17:45:20 +0900

    User と FriendRequest のアソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User < ApplicationRecord
   has_many :messages
   has_one :my_room, class_name: 'Room', foreign_key: 'owner_id'
   has_many :rooms, through: :messages
-  has_one :send_request, class_name: 'FriendRequest', foreign_key: 'sender_id'
-  has_one :receive_request, class_name: 'FriendRequest', foreign_key: 'receiver_id'
+  has_many :send_requests, class_name: 'FriendRequest', foreign_key: 'sender_id'
+  has_many :receive_requests, class_name: 'FriendRequest', foreign_key: 'receiver_id'
+  has_many :receivers, through: :send_requests, source: :receiver
+  has_many :senders, through: :receive_requests, source: :sender
+
   has_one_attached :avatar
 
   validates :name, presence: true
