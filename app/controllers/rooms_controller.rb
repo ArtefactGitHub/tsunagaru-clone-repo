14f3f0fe4a3b382fbad7ec9e5e79f1cc19336520
ArtefactGitHub <artefactgo@gmail.com>
2019-01-17 17:36:02 +0900

    ルームの入室条件を満たしていない場合、メッセージを表示しないように修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,8 @@ class RoomsController < ApplicationController
 
   def show
     @room = Room.find(params[:id])
+    return @messages = [] unless @room.meet_the_requirements?(current_user)
+
     @messages = @room.messages
   end
 end
