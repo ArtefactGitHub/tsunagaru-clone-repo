5e2a94c77491723997d4b990104329f41c12486f
ArtefactGitHub <artefactgo@gmail.com>
2019-01-18 18:08:56 +0900

    ルームに入室出来ない場合、リダイレクトして入れないようにする

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,9 +3,15 @@ class RoomsController < ApplicationController
   layout 'room'
 
   def show
-    @room = Room.find(params[:id])
-    return @messages = [] unless current_user.can_access_room?(@room)
+    @room = Room.find_by(id: params[:id])
+    return redirect_to mypage_root_path, danger: 'ルームが見つかりません' unless can_access_room
 
     @messages = @room.messages
+  end
+
+  private
+
+  def can_access_room
+    @room.present? && current_user.can_access_room?(@room)
   end
 end
