53b7d89995d150cd1b3c27f26622510e8b32d957
ArtefactGitHub <artefactgo@gmail.com>
2019-01-19 16:30:25 +0900

    admin ユーザーは通常のログイン画面からはログイン出来ないようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   require 'securerandom'
 
+  include Role
+
   authenticates_with_sorcery!
 
   has_many :messages, dependent: :destroy
@@ -15,7 +17,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :introduction, length: { maximum: 999 }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
