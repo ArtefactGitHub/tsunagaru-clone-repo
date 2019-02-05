713bd6ccf67593066a892d4ef9fdd2ed259b3633
Artefact <artefactgo@gmail.com>
2019-02-05 11:09:30 +0900

    ルームのメッセージ欄を、コンポーネントの有無で調整する実装

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -17,3 +17,27 @@
 //= require activestorage
 //= require turbolinks
 //= require cable.js
+
+'use strict';
+
+document.addEventListener("turbolinks:load", function(){
+  var window_height = $(window).height();
+  var header_height = $('header').outerHeight();
+  var output_area_height = $('#output-area').outerHeight();
+  var pagenate_area_height = header_height;
+  // 現在のウィンドウの 1rem の高さ
+  // var font_height = $('html').css('font-size');
+  var font_height = $('#message-section-title').height();
+  // メッセージ欄の拡張可能な高さ
+  var add_height = window_height - header_height - output_area_height - pagenate_area_height;
+
+  if($('#text-message-section').length){
+    add_height = add_height - $('#text-message-section').outerHeight();
+	}
+  if($('#button-message-section').length){
+    add_height = add_height - $('#button-message-section').outerHeight();
+	}
+
+  // メッセージ欄の拡張（数rem分調整）
+  $('#message-section').height($('#message-section').outerHeight() + add_height - (font_height * 4));
+})
