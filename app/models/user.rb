30141a305d9fa6518e4edad5683c1a2534c03fd7
Artefact <artefactgo@gmail.com>
2019-02-06 11:02:28 +0900

    パスワードを6文字以上に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 999 }
   validates :email, uniqueness: true, presence: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :uuid, presence: true
