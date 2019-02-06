37c3e266f9c8511d004ef8ee7e05198f950a702a
Artefact <artefactgo@gmail.com>
2019-02-06 15:04:24 +0900

    sorcery の reset_password モジュールを追加 rails g mailer UserMailer reset_password_email rails g sorcery:install reset_password --only-submodules rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_014714) do
+ActiveRecord::Schema.define(version: 2019_02_06_060204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,8 +105,13 @@ ActiveRecord::Schema.define(version: 2019_02_06_014714) do
     t.text "uuid"
     t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
     t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
