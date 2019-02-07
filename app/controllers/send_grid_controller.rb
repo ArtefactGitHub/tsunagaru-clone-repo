2d2398e3fac19af79c8dcb372159d749c42e293e
Artefact <artefactgo@gmail.com>
2019-02-07 22:18:34 +0900

    Revert "SendGrid のドメイン所有者確認対応"
    This reverts commit 6c71c12fa88414669b17df62c4ebc3412363ad67.

--- app/controllers/send_grid_controller.rb
+++
@@ -1,6 +0,0 @@
-class SendGridController < ApplicationController
-  layout 'sendgrid'
-  skip_before_action :require_login
-
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,45 @@
 Rails.application.routes.draw do
-  root to: 'send_grid#index'
+  root to: 'user_sessions#new'
 
-  # root to: 'user_sessions#new'
-  #
-  # if Rails.env.development?
-  #   get '/login_as/:user_id', to: 'development/sessions#login_as'
-  #   get '/messages/clear_messages', to: 'development/messages#clear_messages'
-  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  # end
-  #
-  # get '/login', to: 'user_sessions#new'
-  # post '/login', to: 'user_sessions#create'
-  # delete '/logout', to: 'user_sessions#destroy'
-  #
-  # resources :user_sessions, only: %i[new create destroy]
-  # resources :users, only: %i[new create]
-  # resources :password_resets, only: %i[new create edit update]
-  #
-  # namespace :mypage do
-  #   root to: 'dashboard#show'
-  #
-  #   namespace :user do
-  #     root to: 'users#edit'
-  #     resources :users, only: %i[edit update]
-  #   end
-  #
-  #   namespace :friend do
-  #     root to: 'top#show'
-  #     resources :friends, only: %i[index destroy]
-  #     resources :requests, only: %i[index create destroy]
-  #   end
-  #
-  #   namespace :setting do
-  #     root to: 'top#show'
-  #     resources :message_button_lists, only: %i[edit update]
-  #     resources :use_type_settings, only: %i[edit update]
-  #   end
-  # end
-  #
-  # resources :rooms, only: %i[show] do
-  #   patch 'update_message_button_list', to: 'rooms#update_message_button_list'
-  # end
-  #
-  # # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  if Rails.env.development?
+    get '/login_as/:user_id', to: 'development/sessions#login_as'
+    get '/messages/clear_messages', to: 'development/messages#clear_messages'
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
+
+  resources :user_sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+  resources :password_resets, only: %i[new create edit update]
+
+  namespace :mypage do
+    root to: 'dashboard#show'
+
+    namespace :user do
+      root to: 'users#edit'
+      resources :users, only: %i[edit update]
+    end
+
+    namespace :friend do
+      root to: 'top#show'
+      resources :friends, only: %i[index destroy]
+      resources :requests, only: %i[index create destroy]
+    end
+
+    namespace :setting do
+      root to: 'top#show'
+      resources :message_button_lists, only: %i[edit update]
+      resources :use_type_settings, only: %i[edit update]
+    end
+  end
+
+  resources :rooms, only: %i[show] do
+    patch 'update_message_button_list', to: 'rooms#update_message_button_list'
+  end
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 end
