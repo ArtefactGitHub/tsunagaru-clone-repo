636ef21b02d87200bcf42df84aa1827ab79ee123
Artefact <artefactgo@gmail.com>
2019-02-08 15:52:00 +0900

    定数名の更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,12 +73,12 @@ Rails.application.configure do
 
   # Use SendGrid - Add-ons - Heroku
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: Settings.common.host_name }
+  config.action_mailer.default_url_options = { host: Settings.common.domain }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
-      domain: Settings.common.host_name,
+      domain: Settings.common.domain,
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
