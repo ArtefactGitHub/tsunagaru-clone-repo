a39be4ea705f27ac0eca9d3a814d8bc42cadf940
Artefact <artefactgo@gmail.com>
2019-02-09 09:35:56 +0900

    メッセージ欄の高さ拡張の計算を修正 input_area で計算するようにし、調整後に input_area が画面からはみ出て少しスクロール出来るように高さを加算（モバイル端末でテキスト入力時、キーボードに合わせてスクロールするように）

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -23,26 +23,23 @@ document.addEventListener("turbolinks:load", function() {
 
     function adjust_message_section() {
         var window_height = $(window).height();
-        var header_height = $('header').outerHeight();
-        var output_area_height = $('#output-area').outerHeight();
+        var header_height = $('header').height();
+        var output_area_height = $('#output-area').height();
         var pagenate_area_height = header_height;
+        var input_area_height = $('#input-area').height();
+
         // 現在のウィンドウの 1rem の高さ
         // var font_height = $('html').css('font-size');
         var font_height = $('#message-section-title').height();
         // メッセージ欄の拡張可能な高さ
-        var add_height = window_height - header_height - output_area_height - pagenate_area_height;
-
-        if ($('#text-message-section').length) {
-            add_height = add_height - $('#text-message-section').outerHeight();
-        }
-        if ($('#button-message-section').length) {
-            add_height = add_height - $('#button-message-section').outerHeight();
-        }
+        var add_height = window_height - header_height - output_area_height - pagenate_area_height - input_area_height;
 
         // メッセージ欄の拡張（数rem分調整）
-        add_height = add_height - (font_height * 8);
+        var adjustHeight = (font_height * 2);
+        add_height = add_height - adjustHeight;
         if (add_height > 0) {
           $('#message-section').height($('#message-section').outerHeight() + add_height);
+          $('#input-area').height($('#input-area').outerHeight() + adjustHeight);
         }
     }
 
