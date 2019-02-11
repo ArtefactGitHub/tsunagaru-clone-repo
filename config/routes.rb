1ba58161b8c7c8cc76c6e8dd0d83eff52f8cbb5f
Artefact <artefactgo@gmail.com>
2019-02-11 21:06:41 +0900

    利用規約の雛形

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
 
   get '/about', to: 'about#show_about'
+  get '/term', to: 'term#show'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
