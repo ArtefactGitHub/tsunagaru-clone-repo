ec528350ed2c9a892b8a5209d0dfb6a167441ec3
Artefact <artefactgo@gmail.com>
2019-02-11 10:33:05 +0900

    メンテナンス判定周りの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,11 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :info, :warning, :danger
 
+  # メンテナンス判定（heroku のメンテナンスモードだと管理者ログインが行えないため）
+  # UserSessionsController では個別の判定を行なっている（管理者ログインを行うため）
+  # UsersController では個別の判定を行なっている（デフォルトではメンテナンス中もユーザー作成は行えるため）
   def check_maintenance
-    redirect_if_maintenance
+    return redirect_if_maintenance unless env_can_login?
   end
 
   private
--- app/controllers/concerns/check_maintenance_module.rb
+++ app/controllers/concerns/check_maintenance_module.rb
@@ -5,19 +5,25 @@ module CheckMaintenanceModule
 
   included do
     def redirect_if_maintenance
-      return true if current_user&.admin?
+      return if current_user&.admin?
 
-      return redirect_to about_url, danger: 'メンテナンス中のため、しばらくお待ちください' unless env_can_login?
+      return redirect_to_about
     end
 
+    # ログイン時は current_user が存在しないため、redirect_if_maintenance とは別で判定を行う
+    # （ `unless can_login_with_email(email)` の場合にリダイレクトさせる）
     def redirect_if_maintenance_on_login
-      return redirect_to about_url, danger: 'メンテナンス中のため、しばらくお待ちください'
+      return redirect_to_about
     end
 
     def can_login_with_email?(email)
       return true if login_to_admin_with_email? email
 
       env_can_login?
+    end
+
+    def redirect_to_about
+      return redirect_to about_url, danger: 'メンテナンス中のため、しばらくお待ちください'
     end
 
     def login_to_admin_with_email?(email)
