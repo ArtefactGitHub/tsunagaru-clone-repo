05aaa879db6d69fdca029fa5147fced7aa25a553
Artefact <artefactgo@gmail.com>
2019-02-12 11:59:48 +0900

    バケット名、リージョンは環境毎に分け、アクセスキーなどは Credentials からのみ取得するように修正 （heroku デプロイ時に環境変数がセットされていないとビルド出来ないようなので）

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,10 +10,8 @@ CarrierWave.configure do |config|
                                      Rails.application.credentials.dig(:aws, :bucket_name))
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV.fetch(Settings.env.aws_access_key_id,
-                                   Rails.application.credentials.dig(:aws, :access_key_id)),
-      aws_secret_access_key: ENV.fetch(Settings.env.aws_secret_access_key,
-                                       Rails.application.credentials.dig(:aws, :secret_access_key)),
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
       region: ENV.fetch(Settings.env.aws_region,
                         Rails.application.credentials.dig(:aws, :region)),
       path_style: true
