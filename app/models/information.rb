0903d24d8c764b426666ff9e288a263add208ab7
Artefact <artefactgo@gmail.com>
2019-02-23 12:50:10 +0900

    バリデーションを修正

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,6 +1,7 @@
 class Information < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
+  validates :display_time, presence: true
   validates :title, presence: true
-  validates :content, presence: true
+  # validates :content, presence: true
 end
