068f50d075f09c7dad25a71ce57dbda82fc54c01
Artefact <artefactgo@gmail.com>
2019-02-28 12:44:56 +0900

    Capybara でフルサイズのスクリーンショットを撮るためのモジュールを追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,8 @@ RSpec.configure do |config|
     driven_by :selenium_chrome_headless
   end
 
+  config.include TestHelpers::Spec::Capybara_Extension
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
