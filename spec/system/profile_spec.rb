2d57b63c3f95618333ce112a31aa9808d4f0ea64
Artefact <artefactgo@gmail.com>
2019-03-03 18:27:10 +0900

    プロフィール画面のテストを更新 ・メールアドレスの更新 ・自己紹介文の更新 ・アバター画像の更新

--- spec/system/profile_spec.rb
+++ spec/system/profile_spec.rb
@@ -28,9 +28,63 @@ describe 'プロフィール画面', type: :system do
       end
     end
   end
-end
 
-# 未ログインでプロフィール画面に遷移できない
-# 別のユーザーのプロフィール画面に遷移できない
-# プロフィールが更新出来る
-#
+  describe 'プロフィールの更新' do
+    let(:user_a) { create(:user,
+                          name: 'ユーザーA',
+                          email: 'a@g.com',
+                          image: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/avatar_sample.png'), 'image/jpeg') ) }
+
+    before do
+      login_user(user_a.email, 'password')
+    end
+
+    describe 'メールアドレスの更新' do
+      before do
+        visit mypage_user_path
+        fill_in 'user_email', with: 'a2@g.com'
+        click_button '更新'
+      end
+
+      it 'メールアドレスが更新出来る' do
+        expect(page).to have_field 'user_email', with: 'a2@g.com'
+      end
+      it '正しくリダイレクトされる' do
+        expect(current_path).to eq mypage_user_path
+      end
+    end
+
+    describe '自己紹介文の更新' do
+      before do
+        visit mypage_user_path
+        fill_in 'user_introduction', with: '私はユーザーAです'
+        click_button '更新'
+      end
+
+      it '自己紹介文が更新出来る' do
+        expect(page).to have_field 'user_introduction', with: '私はユーザーAです'
+      end
+      it '正しくリダイレクトされる' do
+        expect(current_path).to eq mypage_user_path
+      end
+    end
+
+    describe 'アバター画像の更新' do
+      before do
+        visit mypage_user_path
+        # attach_file '#user_image', "#{Rails.root}/spec/images/logo.png", {display: 'block'}
+        # attach_file('user[image]', "#{Rails.root}/spec/images/logo.png", make_visible: true)
+        user_a.image = Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/logo.png'), 'image/jpeg')
+        click_button '更新'
+      end
+
+      it 'アバター画像が更新出来る' do
+        expect(user_a.image.url.split('/').last).to eq 'logo.png'
+        expect(page).to have_content 'プロフィールを更新しました'
+      end
+      it '正しくリダイレクトされる' do
+        expect(current_path).to eq mypage_user_path
+      end
+    end
+  end
+end
