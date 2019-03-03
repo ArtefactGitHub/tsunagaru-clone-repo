c1e6a0fecaa727d0643c4143eb475904be5fc3c1
Artefact <artefactgo@gmail.com>
2019-03-03 14:35:25 +0900

    プロフィール画面のテストの修正 ルーティングの修正により、別のユーザーのプロフィール画面を開くことが出来なくなったため

--- spec/system/profile_spec.rb
+++ spec/system/profile_spec.rb
@@ -10,14 +10,14 @@ describe 'プロフィール画面', type: :system do
       end
 
       it 'プロフィール画面に遷移出来る' do
-        visit edit_mypage_user_user_path user_a
+        visit mypage_user_path
         expect(page).to have_content 'プロフィール画面'
       end
     end
 
     context '未ログイン時' do
       before do
-        visit edit_mypage_user_user_path user_a.id
+        visit mypage_user_path
       end
 
       it 'プロフィール画面に遷移出来ない' do
@@ -25,22 +25,6 @@ describe 'プロフィール画面', type: :system do
       end
       it 'ログイン画面に遷移する' do
         expect(page).to have_content 'ログイン'
-      end
-    end
-  end
-
-  describe '別のユーザーのプロフィール画面に遷移できない' do
-    let(:user_a) { create(:user, name: 'ユーザーA', email: 'a@g.com', uuid: 'uuid-a') }
-    let(:user_b) { create(:user, name: 'ユーザーB', email: 'b@g.com', uuid: 'uuid-b') }
-
-    context 'ユーザーAでログイン時' do
-      before do
-        login_user(user_a.email, 'password')
-      end
-
-      it 'ユーザーBのプロフィール画面に遷移出来ない' do
-        visit edit_mypage_user_user_path user_b.id
-        expect(page).not_to have_content 'プロフィール画面'
       end
     end
   end
